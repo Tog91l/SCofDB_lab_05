8d80df66b194b320f002b9103f78e29857a88c49
Tog91l <114440739+Tog91l@users.noreply.github.com>
2026-04-21 16:42:26 +0300

    Update 001_init.sql

--- backend/migrations/001_init.sql
+++ backend/migrations/001_init.sql
@@ -57,10 +57,125 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- TODO: Создать триггер trigger_check_order_not_already_paid
 -- BEFORE UPDATE ON orders FOR EACH ROW
 
+CREATE TABLE order_statuses (
+    status VARCHAR(50) primary key,
+    description VARCHAR(100) not null
+);
+
+INSERT INTO order_statuses (status, description) VALUES
+    ('created' , 'Order create'),
+    ('paid', 'Order paid'),
+    ('cancelled', 'Order cancelled'),
+    ('shipped', 'Order shipped'),
+    ('completed', 'Order completed');
+
+
+
+-- TODO: Создать таблицу order_statuses
+-- Столбцы: status (PK), description
+
+
+-- TODO: Вставить значения статусов
+-- created, paid, cancelled, shipped, completed
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email VARCHAR(250) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CHECK ( email ~* '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.-]+$'),
+    CHECK (email <> '')
+
+);
+-- TODO: Создать таблицу users
+-- Столбцы: id (UUID PK), email, name, created_at
+-- Ограничения:
+--   - email UNIQUE
+--   - email NOT NULL и не пустой
+--   - email валидный (regex через CHECK)
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID not null,
+    status VARCHAR(50) not null, 
+    total_amount DECIMAL(10, 2) NOT NULL DEFAULT 0 CHECK (total_amount >= 0), 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) references users(id),
+    FOREIGN KEY (status)  references order_statuses(status)
+);
+-- TODO: Создать таблицу orders
+-- Столбцы: id (UUID PK), user_id (FK), status (FK), total_amount, created_at
+-- Ограничения:
+--   - user_id -> users(id)
+--   - status -> order_statuses(status)
+--   - total_amount >= 0
+
+
+CREATE TABLE order_items (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_id UUID not null, 
+    product_name VARCHAR(100) not null check (product_name <> ''), 
+    price DECIMAL(10, 2) not null DEFAULT 0 check (price >= 0),
+    quantity integer not null check (quantity > 0),
+
+    FOREIGN KEY (order_id) references orders(id) ON DELETE CASCADE
+
+);
+-- TODO: Создать таблицу order_items
+-- Столбцы: id (UUID PK), order_id (FK), product_name, price, quantity
+-- Ограничения:
+--   - order_id -> orders(id) CASCADE
+--   - price >= 0
+--   - quantity > 0
+--   - product_name не пустой
+
+
+CREATE TABLE order_status_history (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_id UUID not null, 
+    status VARCHAR(50) not null,
+    changed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (order_id) references orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (status) references order_statuses(status)
+);
+
+-- TODO: Создать таблицу order_status_history
+-- Столбцы: id (UUID PK), order_id (FK), status (FK), changed_at
+-- Ограничения:
+--   - order_id -> orders(id) CASCADE
+--   - status -> order_statuses(status)
+
 
 -- ============================================
--- БОНУС (опционально)
+-- КРИТИЧЕСКИЙ ИНВАРИАНТ: Нельзя оплатить заказ дважды
 -- ============================================
--- TODO: Триггер автоматического пересчета total_amount
--- TODO: Триггер автоматической записи в историю при изменении статуса
--- TODO: Триггер записи начального статуса при создании заказа
+-- TODO: Создать функцию триггера check_order_not_already_paid()
+-- При изменении статуса на 'paid' проверить что его нет в истории
+-- Если есть - RAISE EXCEPTION
+CREATE FUNCTION check_order_not_already_paid()
+RETURNS TRIGGER AS $$
+BEGIN 
+    if NEW.status = 'paid' then 
+       if EXISTS (
+        select 1 from order_status_history 
+        where order_id = NEW.id and status = 'paid'
+       ) then 
+          RAISE EXCEPTION 'Order % has already paid', NEW.id;
+        END IF;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- TODO: Создать триггер trigger_check_order_not_already_paid
+-- BEFORE UPDATE ON orders FOR EACH ROW
+CREATE TRIGGER trigger_check_order_not_already_paid
+    BEFORE UPDATE ON orders
+    FOR EACH ROW
+    EXECUTE FUNCTION check_order_not_already_paid();
+
