0a6130291068f4ff6976ab036d5d13f81f0f0e99
Tog91l <114440739+Tog91l@users.noreply.github.com>
2026-04-21 16:33:57 +0300

    Update user.py

--- backend/app/domain/user.py
+++ backend/app/domain/user.py
@@ -1,9 +1,11 @@
 """Доменная сущность пользователя."""
 
 import uuid
-from datetime import datetime
-
+import re
+from datetime import datetime, timezone
+from dataclasses import dataclass, field
 from .exceptions import InvalidEmailError
+
 
 
 # TODO: Реализовать класс User
@@ -12,5 +14,22 @@ from .exceptions import InvalidEmailError
 # - Реализовать валидацию email в __post_init__
 # - Regex: r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
+em_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.-]+$"
+def valid_email(email: str): 
+    email = email.strip()
+    if not email or not re.match(em_regex, email):
+        raise InvalidEmailError(email)
+    return email
+
+
+
+@dataclass
 class User:
-    pass
+    email: str 
+    name: str = ""
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+
+    def __post_init__(self):
+        self.email = valid_email(self.email)
+
